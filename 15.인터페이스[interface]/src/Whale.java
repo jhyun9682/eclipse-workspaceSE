0a03feb7ff1559471decb6d262f3f2b96563aeb5
academyitwill <jhyun9682@naver.com>
2022-06-07 10:39:41 +0900

    ..

--- 15.인터페이스[interface]/src/Whale.java
+++ 15.인터페이스[interface]/src/Whale.java
@@ -1,3 +1,21 @@
 public class Whale implements Mammalia {
 
+	@Override
+	public void operation1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void operation2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void operation3() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
