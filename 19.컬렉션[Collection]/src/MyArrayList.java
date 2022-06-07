0ecb51ec72c8510acfbe6a73811b3e2a72cc61b4
academyitwill <jhyun9682@naver.com>
2022-06-07 11:57:58 +0900

    ..

--- 19.컬렉션[Collection]/src/MyArrayList.java
+++ 19.컬렉션[Collection]/src/MyArrayList.java
@@ -3,5 +3,11 @@ import java.util.ArrayList;
 public class MyArrayList {
 	
 	private Object[] elementData;
+	public MyArrayList() {
+	elementData=new Object[0];	
+	}
+	public void add() {
+		
+	}
 
 }
