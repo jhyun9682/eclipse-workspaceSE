17c1961d5fd9aaf358ce407f8fc2bb42a34066bf
academyitwill <jhyun9682@naver.com>
2022-06-07 11:40:01 +0900

    ..

---
+++ 19.컬렉션[Collection]/src/ObjectArrayMain.java
@@ -0,0 +1,16 @@
+
+public class ObjectArrayMain {
+
+	public static void main(String[] args) {
+		Object o1=new Account(1111,"BING",3300,0.5);
+		Object o2=new Account(2222,"KING",2300,0.1);
+		Object o3=new Account(3333,"KING",5600,0.2);
+		Object o4=new Account(4444,"YING",8900,0.5);
+		Object o5=new Account(5555,"SING",9900,0.8);
+		
+		Object[] objectTypeArray=new Object[5];
+		
+
+	}
+
+}
