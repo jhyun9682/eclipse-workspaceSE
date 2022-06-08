17436ef0d9e0eb973afa6a04e399e237c6a14003
academyitwill <jhyun9682@naver.com>
2022-06-08 17:44:31 +0900

    ..

--- 99.JAVA_PRACTICE/src/com/itwill06/collection/car/CarHashMapMain.java
+++ 99.JAVA_PRACTICE/src/com/itwill06/collection/car/CarHashMapMain.java
@@ -1,6 +1,12 @@
 package com.itwill06.collection.car;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+
+
+
 
 public class CarHashMapMain {
 
@@ -15,26 +21,52 @@ public class CarHashMapMain {
 		Car c7 = new Car("5656",9 );
 		Car c8 = new Car("7789",10 );
 		Car c9 = new Car("4690",11 );
-		/*
-		 * 0.Car HashMap 객체생성 초기화
-		 */
-		HashMap<String,Car> carMap=new HashMap<String,Car>();		
+		// 0.Car HashMap 객체생성 초기화
+		HashMap<String,Car> carMap=new HashMap<String,Car>();	
+		
+		
 		System.out.println("1.입차 9대");
-		/*
-		 * 1.차량객체생성
-		 * 2.carMap저장
-		 */
+		//1.차량객체생성
+		carMap.put("2344",new Car("2344",3));
+		carMap.put("4566",new Car("4566",4));
+		carMap.put("4567",new Car("4567",5));
+		carMap.put("2389",new Car("2389",6));
+		carMap.put("3908",new Car("3908",7));
+		carMap.put("7891",new Car("7891",8));
+		carMap.put("5656",new Car("5656",9));
+		carMap.put("7789",new Car("7789",10));
+		carMap.put("4690",new Car("4690",11));
+		 // 2.carMap저장
+		System.out.println(carMap);
+		 
 		System.out.println("2.차량번호 7789번  차한대 정보출력");
+		Car findCar=carMap.get("7789");
+		findCar.print();
+		
 		System.out.println("3.입차시간 8시이후 차량여러대 찾아서 정보출력");
-		System.out.println("4.2389번차량 12시 출차");
-		/*
-		 * 1. 2389번차량찾기
-		 * 2. 출차
-		 * 3. 영수증출력
-		 * 4. 2389번차량 주차장에서 차량삭제
-		 */
-		System.out.println("5. 2389번차량 출차후전체 차량출력");
-
+	//???
+		
+		System.out.println("4.7789번차량 12시 출차");
+		//1. 7789번차량찾기
+		//2. 출차
+		//3. 영수증출력
+		//4. 2389번차량 주차장에서 차량삭제
+		Car outCar=carMap.remove("7789");
+		outCar.setOutTime(12);
+		outCar.calculateFee();
+		outCar.print();
+	
+	
+		 
+		System.out.println("5. 7789번차량 출차후전체 차량출력");
+		Set<String> keySet=carMap.keySet();
+		Iterator<String> keyIterator=keySet.iterator();
+		while(keyIterator.hasNext()) {
+			
+			String key=keyIterator.next();
+			Car tempCar= carMap.get(key);
+			tempCar.print();
+		}
 	}
 
 }
--- 99.JAVA_PRACTICE/src/com/itwill06/collection/student/StudentArrayListMain.java
+++ 99.JAVA_PRACTICE/src/com/itwill06/collection/student/StudentArrayListMain.java
@@ -2,12 +2,13 @@ package com.itwill06.collection.student;
 
 import java.util.ArrayList;
 
+
 public class StudentArrayListMain {
 
 	public static void main(String[] args) {
-		/*
-		 * 0.학생ArrayList객체생성초기화
-		 */
+		
+		 //0.학생ArrayList객체생성초기화
+		 
 		ArrayList<Student> studentList=new ArrayList<Student>();
 		studentList.add(new Student(1,"KIM",89,93,94));
 		studentList.add(new Student(2,"HIM",88,77,98));
@@ -18,56 +19,118 @@ public class StudentArrayListMain {
 		studentList.add(new Student(7,"KIM",99,93,95));
 		studentList.add(new Student(8,"LIM",83,80,99));
 		studentList.add(new Student(9,"QIM",73,90,80));
-		/*
-		 * 0.학생두명추가
-		 */
 		
-		/*
-		 * 1. 전체학생총점,평균,평점계산
-		 */
+		//0.학생두명추가 
+		System.out.println("0.학생 2명 추가");
+		//accountList.add(1, new Account(1112, "NEW", 52000, 1.1));
+		studentList.add(9,new Student(10,"NNN",50,50,50));
+		studentList.add(10,new Student(11,"WWW",60,60,60));
+		
+		// 1. 전체학생총점,평균,평점계산
 		System.out.println("1. 전체학생총점,평균,평점계산");
-		/*
-		 * 2. 전체학생 총점으로 석차계산
-		 */
+		for (Student student : studentList) {
+			student.calculateTotal();
+			student.calculateAvg();
+			student.calculateGrade();
+		}
+		
+		//2. 전체학생 총점으로 석차계산???
 		System.out.println("2. 전체학생 총점으로 석차계산");
-		/*
-		 * 3. 전체학생출력
-		 */
+		for (int i = 0; i < studentList.size(); i++) {
+			for (int j = 0; j < studentList.size(); j++) {
+			
+				}
+		}
+		
+		// 3. 전체학생출력
 		System.out.println("3. 전체학생출력 (enhanced for)");
 		Student.headerPrint();
-		/*
-		 * 4. 번호3번 학생한명 출력
-		 */
+		for (Student student : studentList) {
+				student.print();
+		}
+		
 		System.out.println("4.번호3번 학생한명 출력  ");
-		/*
-		 * 5. 학점A인 학생들 출력
-		 */
+		 //4. 번호3번 학생한명 출력
+		for (Student student : studentList) {
+			if(student.getNo()==3) {
+				student.print();
+				break;
+			}
+			
+		}
+		
+
+		
+		// 5. 학점A인 학생들 출력
 		System.out.println("5. 학점A인 학생들 출력");
-		/*
-		 6.이름이 KIM인학생들 출력
-		 */
+		for (Student student : studentList) {
+			if(student.getGrade()=='A') {
+				student.print();
+				
+			}
+			
+		}
+		
+		// 6.이름이 KIM인학생들 출력
 		System.out.println("6.이름이 KIM인학생들 출력");
-		/*
-		 * 7.번호3번학생한명삭제
-		 */
+		for (Student student : studentList) {
+			if(student.getName().equals("KIM")) {
+				student.print();
+				
+			}
+			
+		}
+		
+		
+		// 7.번호3번학생한명삭제
 		System.out.println("7.번호3번학생삭제");
-		/*
-		 * 8.이름KIM인 학생들삭제
-		 */
+		for (Student student : studentList) {
+			if(student.getNo()==3) {
+				boolean isRemove =studentList.remove(student);
+				if(isRemove) {
+					
+					student.print();
+				}
+				break;
+			}
+			
+		}
+		
+		
+		// 8.이름KIM인 학생들삭제
 		System.out.println("8.이름KIM인 학생들삭제");
-		/*
-		 * 9.평점 F 인 학생들삭제
-		 */
+		for (int i = 0; i < studentList.size(); i++) {
+			if(studentList.get(i).getName().equals("KIM")){
+				studentList.remove(i).print();
+				i--;
+				
+			}
+		}
+		
+		// 9.평점 F 인 학생들삭제
 		System.out.println("9.평점 F 인 학생들삭제");
+		for (int i = 0; i < studentList.size(); i++) {
+			if(studentList.get(i).getGrade()=='F') {
+				studentList.remove(i).print();
+				i--;
+			}
+		}
+		
+		System.out.println("3. 전체학생출력 (enhanced for)");
+		Student.headerPrint();
+		for (Student student : studentList) {
+				student.print();
+		}
+		
 		/******************안해도됨******************************/
 		/*
 		 * 10. 학생총점으로 오름차순정렬
-		 */
 		System.out.println("10. 학생총점으로 내림차순정렬");
+		 */
 		/*
 		  11. 학생이름순으로 오름차순정렬
-		 */
 		System.out.println("11.학생이름순으로 오름차순정렬");
+		 */
 		/********************************************************/
 
 	}
