d4316f799e7282afce579e2efc6462ac54e65a2a
academyitwill <jhyun9682@naver.com>
2022-06-09 15:22:05 +0900

    ..

--- 20.스레드[Thread]/src/ConCurrentBeep.java
+++ 20.스레드[Thread]/src/ConCurrentBeep.java
@@ -1,4 +1,19 @@
+import java.awt.Toolkit;
 
-public class ConCurrentBeep {
+public class ConCurrentBeep extends Thread {
+	
+	public void run() {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		for (int i = 0; i < 10; i++) {
+			try {
+				toolkit.beep();
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+
+		}
+
+	}
 
 }
--- 20.스레드[Thread]/src/ConCurrentPrint.java
+++ 20.스레드[Thread]/src/ConCurrentPrint.java
@@ -1,4 +1,15 @@
 
-public class ConCurrentPrint {
+public class ConCurrentPrint  extends Thread{
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			try {
+				System.out.println("띵");
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			
+		}
+	}
 
 }
