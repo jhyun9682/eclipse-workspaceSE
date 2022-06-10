7dd44187843a1d93e0403ec23f6429e8bf05ffa2
academyitwill <jhyun9682@naver.com>
2022-06-10 11:44:43 +0900

    ..

--- 19.컬렉션[Collection]/src/generic/HashSetStringWrapperMain.java
+++ 19.컬렉션[Collection]/src/generic/HashSetStringWrapperMain.java
@@ -1,9 +1,33 @@
 package generic;
 
+import java.util.HashSet;
+
 public class HashSetStringWrapperMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("######### String ########");
+		HashSet<String> nameSet=new HashSet<String>();
+		nameSet.add("KIM");
+		nameSet.add("JIM");
+		nameSet.add("GIM");
+		nameSet.add("SIM");
+		System.out.println(nameSet);
+		boolean isAdd=nameSet.add("KIM");
+		System.out.println("추가여부:"+isAdd);
+		System.out.println(nameSet);
+		
+		System.out.println("###### Wrapper ######");
+		HashSet <Integer> lottoSet=new HashSet<Integer>();
+		System.out.println(lottoSet);
+		lottoSet.add(new Integer(34));
+		lottoSet.add(23);
+		lottoSet.add(12);
+		lottoSet.add(new Integer(43));
+		lottoSet.add(1);
+		System.out.println(lottoSet);
+		isAdd=lottoSet.add(34);
+		System.out.println("추가여부:"+isAdd);
+		System.out.println(lottoSet);
 
 	}
 
