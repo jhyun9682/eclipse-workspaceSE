f1614aec2129e6c2be099207b42a671c471c587c
academyitwill <jhyun9682@naver.com>
2022-06-10 11:31:30 +0900

    ..

---
+++ 19.컬렉션[Collection]/src/generic/ArrayListStringWrapperMain.java
@@ -0,0 +1,64 @@
+package generic;
+
+import java.util.ArrayList;
+
+public class ArrayListStringWrapperMain {
+
+	public static void main(String[] args) {
+		System.out.println("###############Sting###########");
+		ArrayList<String> nameList=new ArrayList<String>();
+		System.out.println("----1.add-----");
+		nameList.add("KIM");
+		nameList.add("JIM");
+		nameList.add("HIM");
+		nameList.add("AIM");
+		nameList.add("TIM");
+		nameList.add("BIM");
+		nameList.add("CIM");
+		nameList.add(new String("KIM"));
+		System.out.println(nameList);
+		System.out.println("----2.get-----");
+		System.out.println(nameList.get(1));
+		System.out.println(nameList.get(3));
+		System.out.println(nameList.get(5));
+		System.out.println("----3.remove(index)-----");
+		nameList.remove(1);
+		System.out.println(nameList);
+		System.out.println("----4.remove(String)-----");
+		nameList.remove("TIM");
+		System.out.println(nameList);
+		nameList.remove("KIM");
+		//Removes the first occurrence of the specified element from this list
+		System.out.println(nameList);
+		for (int i = 0; i < nameList.size(); i++) {
+			System.out.println(nameList.get(i));
+			
+		}
+		System.out.println("############## Wrapper ########");
+		ArrayList<Integer> scoreList=new ArrayList<Integer>();
+		scoreList.add(90);
+		scoreList.add(30);
+		scoreList.add(45);
+		scoreList.add(78);
+		scoreList.add(89);
+		scoreList.add(12);
+		scoreList.add(new Integer(100));
+		scoreList.add(new Integer(45));
+		System.out.println(scoreList);
+		
+		System.out.println("---------1.get------");
+		Integer getScore= scoreList.get(2);
+		int score=getScore.intValue();
+		System.out.println(score);
+		int score2=scoreList.get(3);
+		System.out.println(score2);
+		System.out.println("---------2.set------");
+		scoreList.set(2, 99);
+		System.out.println(scoreList);
+		System.out.println("---------3.remove------");
+		scoreList.remove(5);
+		System.out.println(scoreList);
+
+	}
+
+}
