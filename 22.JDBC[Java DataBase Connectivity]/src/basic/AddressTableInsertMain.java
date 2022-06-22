a2232e4ebf17cb46e42d83e63c703ed358419714
academyitwill <jhyun9682@naver.com>
2022-06-22 15:45:02 +0900

    ..

--- 22.JDBC[Java DataBase Connectivity]/src/basic/AddressTableInsertMain.java
+++ 22.JDBC[Java DataBase Connectivity]/src/basic/AddressTableInsertMain.java
@@ -1,9 +1,35 @@
 package basic;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class AddressTableInsertMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception{
+		/*
+		String driverClass="oracle.jdbc.OracleDriver";
+		String url="jdbc:oracle:thin:@localhost:1521:xe";
+		String user="java";
+		String password="java";
+		*/
+		
+		String insertSQL="insert into address values(address_no_seq.nextval,'orange'||address_no_seq.nextval,'오렌지','890-9090','서울시민')";
+				
+		String driverClass="oracle.jdbc.OracleDriver";
+		String url="jdbc:oracle:thin:@182.237.126.19:1521:xe";
+		String user="jdeveloper00";
+		String password="jdeveloper00";
+		
+		Class.forName(driverClass);
+		Connection con=DriverManager.getConnection(url,user,password);
+		Statement stmt=con.createStatement();
+		
+		int insertRowCount= stmt.executeUpdate(insertSQL);
+		System.out.println(">>"+insertRowCount+"행 insert");
+		stmt.close();
+		con.close();
+		
 
 	}
 
--- 22.JDBC[Java DataBase Connectivity]/src/basic/AddressTableUpdateByPrimaryKeyMain.java
+++ 22.JDBC[Java DataBase Connectivity]/src/basic/AddressTableUpdateByPrimaryKeyMain.java
@@ -1,10 +1,27 @@
 package basic;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class AddressTableUpdateByPrimaryKeyMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception{
+		String driverClass="oracle.jdbc.OracleDriver";
+		String url="jdbc:oracle:thin:@182.237.126.19:1521:xe";
+		String user="jdeveloper00";
+		String password="jdeveloper00";
+		
+		String updateSQL="update address set name='제임스',phone='333-3333',address='서울시 강남구' where no = 1";
+		
+		Class.forName(driverClass);
+		Connection con=DriverManager.getConnection(url,user,password);
+		Statement stmt=con.createStatement();
+		
+		int updateRowCount= stmt.executeUpdate(updateSQL);
+		System.out.println(">>"+updateRowCount+"행 update");
+		stmt.close();
+		con.close();
 	}
 
 }
