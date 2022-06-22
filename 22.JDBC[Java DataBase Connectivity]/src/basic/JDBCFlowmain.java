831edf7c13a1aacdb0eea9fbeded8a4bfb5a5186
academyitwill <jhyun9682@naver.com>
2022-06-22 11:46:04 +0900

    ..

---
+++ 22.JDBC[Java DataBase Connectivity]/src/basic/JDBCFlowmain.java
@@ -0,0 +1,24 @@
+package basic;
+
+public class JDBCFlowmain {
+
+	public static void main(String[] args) {
+		/*1.Driver(Oracle) Class loading */
+		
+		/*2.Connection 객체 생성
+		 * -DB Server 연결객체
+		 */
+		
+		/*3.Statement 객체 생성
+		 * -SQL문 전송객체 생성
+		 * */
+		
+		/*4.SQL전송 (select) */
+		
+		/*5.Resultset 결과얻기
+		 * -결과얻기
+		 * */
+
+	}
+
+}
