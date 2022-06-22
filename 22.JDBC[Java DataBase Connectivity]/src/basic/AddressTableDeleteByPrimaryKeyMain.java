6f68243c278df991f4908c4bc6f75437fd555cfd
academyitwill <jhyun9682@naver.com>
2022-06-22 15:53:55 +0900

    ..

--- 22.JDBC[Java DataBase Connectivity]/src/basic/AddressTableDeleteByPrimaryKeyMain.java
+++ 22.JDBC[Java DataBase Connectivity]/src/basic/AddressTableDeleteByPrimaryKeyMain.java
@@ -1,9 +1,27 @@
 package basic;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class AddressTableDeleteByPrimaryKeyMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception{
+		String driverClass="oracle.jdbc.OracleDriver";
+		String url="jdbc:oracle:thin:@182.237.126.19:1521:xe";
+		String user="jdeveloper00";
+		String password="jdeveloper00";
+		
+		String deleteSQL="delete address where no=19";
+		
+		Class.forName(driverClass);
+		Connection con=DriverManager.getConnection(url,user,password);
+		Statement stmt=con.createStatement();
+		
+		int deleteRowCount= stmt.executeUpdate(deleteSQL);
+		System.out.println(">>"+deleteRowCount+"행 update");
+		stmt.close();
+		con.close();
 
 	}
 
