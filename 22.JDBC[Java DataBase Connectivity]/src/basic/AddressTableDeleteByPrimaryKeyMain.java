f3a8542b50c0107aaedc664708b25b4b25562cbf
academyitwill <jhyun9682@naver.com>
2022-06-22 14:54:26 +0900

    ..

--- 22.JDBC[Java DataBase Connectivity]/src/basic/AddressTableDeleteByPrimaryKeyMain.java
+++ 22.JDBC[Java DataBase Connectivity]/src/basic/AddressTableDeleteByPrimaryKeyMain.java
@@ -1,6 +1,6 @@
 package basic;
 
-public class AddressTableUpdateByPrimarykeyMain {
+public class AddressTableDeleteByPrimaryKeyMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- 22.JDBC[Java DataBase Connectivity]/src/basic/AddressTableUpdateByPrimaryKeyMain.java
+++ 22.JDBC[Java DataBase Connectivity]/src/basic/AddressTableUpdateByPrimaryKeyMain.java
@@ -1,6 +1,6 @@
 package basic;
 
-public class AddressTableDeleteByPrimarykeyMain {
+public class AddressTableUpdateByPrimaryKeyMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
