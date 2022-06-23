73cfee16303d6ee67db309ba4937e5af2f8a5ca2
academyitwill <jhyun9682@naver.com>
2022-06-23 11:24:49 +0900

    ..

---
+++ 22.JDBC[Java DataBase Connectivity]/src/dao/address/first/AddressTableCRUDClassMain.java
@@ -0,0 +1,16 @@
+package dao.address.first;
+
+public class AddressTableCRUDClassMain {
+
+	public static void main(String[] args) throws Exception{
+		AddressTableCreateReadUpdateDeleteClass aa= new AddressTableCreateReadUpdateDeleteClass();
+		aa.a();
+		aa.b();
+		aa.c();
+		aa.d();
+		aa.e();
+	
+
+	}
+
+}
---
+++ 22.JDBC[Java DataBase Connectivity]/src/dao/address/first/AddressTableCreateReadUpdateDeleteClass.java
@@ -0,0 +1,138 @@
+package dao.address.first;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class AddressTableCreateReadUpdateDeleteClass {
+	public void a() throws Exception{
+		
+		String driverClass="oracle.jdbc.OracleDriver";
+		String url="jdbc:oracle:thin:@182.237.126.19:1521:xe";
+		String user="jdeveloper00";
+		String password="jdeveloper00";
+		
+		String insertSQL="insert into address values(address_no_seq.nextval,'orange'||address_no_seq.nextval,'오렌지','890-9090','서울시민')";
+		Class.forName(driverClass);
+		Connection con=DriverManager.getConnection(url,user,password);
+		Statement stmt=con.createStatement();
+		
+		int insertRowCount= stmt.executeUpdate(insertSQL);
+		System.out.println(">>"+insertRowCount+"행 insert");
+		stmt.close();
+		con.close();
+	}
+	public void b() throws Exception{
+		String driverClass="oracle.jdbc.OracleDriver";
+		String url="jdbc:oracle:thin:@182.237.126.19:1521:xe";
+		String user="jdeveloper00";
+		String password="jdeveloper00";
+		
+		String updateSQL="update address set name='제임스',phone='333-3333',address='서울시 강남구' where no = 1";
+		
+		Class.forName(driverClass);
+		Connection con=DriverManager.getConnection(url,user,password);
+		Statement stmt=con.createStatement();
+		
+		int updateRowCount= stmt.executeUpdate(updateSQL);
+		System.out.println(">>"+updateRowCount+"행 update");
+		stmt.close();
+		con.close();
+	}
+	public void c() throws Exception{
+		String driverClass="oracle.jdbc.OracleDriver";
+		String url="jdbc:oracle:thin:@182.237.126.19:1521:xe";
+		String user="jdeveloper00";
+		String password="jdeveloper00";
+		
+		String deleteSQL="delete address where no=19";
+		
+		Class.forName(driverClass);
+		Connection con=DriverManager.getConnection(url,user,password);
+		Statement stmt=con.createStatement();
+		
+		int deleteRowCount= stmt.executeUpdate(deleteSQL);
+		System.out.println(">>"+deleteRowCount+"행 update");
+		stmt.close();
+		con.close();
+	}
+	public void d() throws Exception{
+		String driverClass="oracle.jdbc.OracleDriver";
+		String url="jdbc:oracle:thin:@182.237.126.19:1521:xe";
+		String user="jdeveloper00";
+		String password="jdeveloper00";
+		//
+		String selectSQL=" select no,id,name,phone,address from address where no = 1";
+		//
+		Class.forName(driverClass);
+		Connection con=DriverManager.getConnection(url,user,password);
+		Statement stmt=con.createStatement();
+		ResultSet rs=stmt.executeQuery(selectSQL);
+		//
+		
+		if(rs.next()) {
+			/*
+			 * DB타입 		 | 자바타입
+			 * number 		 | int,double
+			 * varchar2,char | String
+			 * Date 		 | Date
+			 * 
+			 *  XXX data=rs.getXXX("컬럼이름");
+			 */
+			int no=rs.getInt("no");
+			String id=rs.getString("id");
+			String name=rs.getString("name");
+			String phone=rs.getString("Phone");
+			String address=rs.getString("address");
+			System.out.println(no+"\t"+name+"\t"+phone+"\t"+address);
+		}else{
+			System.out.println("조건 만족하는 주소록 없음");
+			
+		}
+		rs.close();
+		stmt.close();
+		con.close();
+	}
+	public void e() throws Exception{
+		String driverClass="oracle.jdbc.OracleDriver";
+		String url="jdbc:oracle:thin:@182.237.126.19:1521:xe";
+		String user="jdeveloper00";
+		String password="jdeveloper00";
+		//
+		String selectSQL=" select no,id,name,phone,address from address";
+		//
+		Class.forName(driverClass);
+		Connection con=DriverManager.getConnection(url,user,password);
+		Statement stmt=con.createStatement();
+		ResultSet rs=stmt.executeQuery(selectSQL);
+		//
+				
+		if(rs.next()) {
+		do {
+			/*
+			 * DB타입 		 | 자바타입
+			 * number 		 | int,double
+			 * varchar2,char | String
+			 * Date 		 | Date
+			 * 
+			 *  XXX data=rs.getXXX("컬럼이름");
+			 */
+			int no=rs.getInt("no");
+			String id=rs.getString("id");
+			String name=rs.getString("name");
+			String phone=rs.getString("Phone");
+			String address=rs.getString("address");
+			System.out.println(no+"\t"+name+"\t"+phone+"\t"+address);
+		}while(rs.next());
+		}else {
+			System.out.println("조건 만족하는 주소록 없음");
+		}
+		
+		rs.close();
+		stmt.close();
+		con.close();
+	}
+
+
+}
