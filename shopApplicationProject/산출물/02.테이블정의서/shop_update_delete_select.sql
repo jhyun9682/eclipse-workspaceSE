67e0fa3d7057255b5241194e14d81355d3590034
academyitwill <jhyun9682@naver.com>
2022-06-29 15:36:47 +0900

    ..

--- shopApplicationProject/산출물/02.테이블정의서/shop_update_delete_select.sql
+++ shopApplicationProject/산출물/02.테이블정의서/shop_update_delete_select.sql
@@ -33,8 +33,13 @@ delete from cart where cart_no=1;
 --로그인한 멤버(guard1)의 카트아이템 제품1개의 수량변경(pk)
 update cart set cart_qty= 8 where cart_no=2;
 
---로그인한 멤버(guard1)의 카트에 제품추가시에 수정
+--로그인한 멤버(guard1)의 카트아이템 리스트에 제품 수(1>변경,0>insert)
+select count(*) as "cnt" from cart where userid='guard1' and p_no=7;
+select count(*) as "cnt" from cart where userid='guard1' and p_no=1;
 
+
+--로그인한 멤버(guard1)의 카트에 제품추가시에 수정(이미 제품존재)
+update cart set cart_qty=cart_qty+1 where p_no=7 and userid='guard1';
 
 
 
