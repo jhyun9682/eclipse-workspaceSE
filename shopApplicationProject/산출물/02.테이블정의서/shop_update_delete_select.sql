23805782f08f2388c14647cb0138167a14f8f5cb
academyitwill <jhyun9682@naver.com>
2022-06-29 14:48:10 +0900

    ..

--- shopApplicationProject/산출물/02.테이블정의서/shop_update_delete_select.sql
+++ shopApplicationProject/산출물/02.테이블정의서/shop_update_delete_select.sql
@@ -14,9 +14,17 @@ delete userinfo where userid='guard4';
 --제품리스트
 select * from product;
 --제품상세보기
-select*from product where p_no=1;
+select * from product where p_no=1;
 --제품수정(X)
 --제품삭제(X)
 --제품추가(X)
 
 /*****cart*****/
+--로그인한 멤버(guard1)의 카트리스트(cart)
+select * from cart where userid='guard1';
+--로그인한 멤버(guard1)의 카트리스트(cart+product)
+select * from cart c join product p on c.p_no=p.p_no where userid='guard1';
+--로그인한 멤버(guard2)의 카트리스트(cart+product)
+select * from cart c join product p on c.p_no=p.p_no where userid='guard2';
+--
+
