121845056881dcaaf4c9785f10dcfa3cd0161e9a
academyitwill <jhyun9682@naver.com>
2022-06-29 14:15:33 +0900

    ..

---
+++ shopApplicationProject/산출물/02.테이블정의서/shop_insert.sql
@@ -0,0 +1,20 @@
+--insert
+
+/***user insert***/
+insert into userinfo(userid,password,name,email) values('guard1','1111','김경호1','guard1@korea.com');
+insert into userinfo(userid,password,name,email) values('guard2','2222','김경호2','guard2@korea.com');
+insert into userinfo(userid,password,name,email) values('guard3','3333','김경호3','guard3@korea.com');
+insert into userinfo(userid,password,name,email) values('guard4','4444','김경호4','guard4@korea.com');
+
+/***product insert***/
+insert into product values(1, '비글', 550000, 'bigle.png','귀여워요...', 0);
+insert into product values(2, '달마시안', 500000, 'dalma.jpg','사랑스러워요', 0);
+insert into product values(3, '퍼그', 400000, 'pug.jpg','예뻐요', 0);
+insert into product values(4, '페키니즈', 450000, 'pekiniz.png','든든해요.', 0);
+insert into product values(5, '포메라니안', 800000, 'pomeranian.jpg','키우기쉬워요.', 0);
+insert into product values(6, '샤페이', 700000, 'shaipei.jpg','애교가많아요.', 0);
+insert into product values(7, '닥스훈트', 800000, 'dachshund.jpg','멋있어요.', 0);
+insert into product values(8, '사모예드', 800000, 'samoyed.jpg','용감해요.', 0);
+
+
+commit;
