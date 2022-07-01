b22d306420e63f544dc38643eb88dedf0a4379d8
academyitwill <jhyun9682@naver.com>
2022-07-01 19:57:26 +0900

    ..

---
+++ 99.JAVA_PRACTICE/src/ReservationInfoDaoTest.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+
+public class ReservationInfoDaoTest {
+
+	public static void main(String[] args) throws Exception {
+			
+		//CartDao cartDao=new CartDao();
+			System.out.println("1.add(insert)");
+			
+			int rowCount=-999;
+			/*
+			rowCount = cartDao.add(addCart);
+			System.out.println(">> "+rowCount);
+			*/
+			/*
+			System.out.println("2.updateByCartNo");
+			rowCount=cartDao.update(8,5);
+			System.out.println(">> "+rowCount);
+			
+			System.out.println("2.updateByProductNoAndUserId");
+			rowCount=cartDao.update("guard1",3,2);
+			System.out.println(">> "+rowCount);
+			
+			System.out.println("3.delete");
+			rowCount=cartDao.deleteCartByNo(8);
+			System.out.println(">> "+rowCount);
+			System.out.println("4.cartList[select]");
+			List<CartItem> cartList1=cartDao.getCartList("guard1");
+			System.out.println("guard1-->"+cartList1);
+			List<CartItem> cartList2=cartDao.getCartList("guard2");
+			System.out.println("guard2-->"+cartList2);
+			List<CartItem> cartList3=cartDao.getCartList("guard3");
+			System.out.println("guard3-->"+cartList3);
+			System.out.println("5.selectProductCount");
+			int productCount1=cartDao.selectProductCount("guard1",8 );
+			System.out.println(">> "+productCount1);
+			int productCount2=cartDao.selectProductCount("guard1",3 );
+			System.out.println(">> "+productCount2);
+			*/
+		}
+	
+
+	}
+
