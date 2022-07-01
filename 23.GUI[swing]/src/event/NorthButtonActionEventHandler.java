0f3455be6fcad3c93e2edff1ab995fef0fd5c111
academyitwill <jhyun9682@naver.com>
2022-07-01 11:31:21 +0900

    ..

--- 23.GUI[swing]/src/event/NorthButtonActionEventHandler.java
+++ 23.GUI[swing]/src/event/NorthButtonActionEventHandler.java
@@ -8,6 +8,10 @@ public class NorthButtonActionEventHandler implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.out.println("north button click...");
+		/*
+		 * 프레임의 타이틀변경
+		 * contentPane의 배경색변경
+		 */
 
 		
 	}
